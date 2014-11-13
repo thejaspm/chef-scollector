2cbd813514d9691d36907cd29fc0a9f96ce1d12d
thejaspm <thejas.p.murthy@gmail.com>
2014-11-13 03:24:38 -0800

    metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'chefi-scollector'
+maintainer       'Thejas'
+maintainer_email 'thejas.p.murthy@gmail.com'
+license          'Apache license'
+description      'Installs/Configures bosun scollector'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1'
+
+supports 'ubuntu'
